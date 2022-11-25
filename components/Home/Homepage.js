e9ce0e84f29622554638aa089e773b7cc947835b
farjanaabedin <farjanaabedin579@gmail.com>
2022-11-25 21:53:39 +0600

    first commit

---
+++ components/Home/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Banner from "./Banner";
+import Products from "./products";
+import BestProduct from "./BestProduct";
+import Footer from "./footer";
+const HomePage = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Products></Products>
+      <BestProduct></BestProduct>
+     
+    </div>
+  );
+};
+
+export default HomePage;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Footer from "../components/Home/footer";
+import Navigation from "../components/Home/Navigation";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navigation></Navigation>
+      <Component {...pageProps} />
+      <Footer></Footer>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;

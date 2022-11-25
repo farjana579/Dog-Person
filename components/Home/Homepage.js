490c43cb197cd7afd0efe31322a2dc609897f6e5
tashiq <fahim.csecu@gmail.com>
2022-11-26 00:24:21 +0600

    testing whether collapse

--- components/Home/Homepage.js
+++ components/Home/Homepage.js
@@ -2,14 +2,12 @@ import React from "react";
 import Banner from "./Banner";
 import Products from "./products";
 import BestProduct from "./BestProduct";
-import Footer from "./footer";
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
       <Products></Products>
       <BestProduct></BestProduct>
-     
     </div>
   );
 };

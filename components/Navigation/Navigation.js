9747ae046454ed76e8ce9474a676eac8191d43d7
EituPaul <zitupaul@gmail.com>
2022-11-27 12:39:26 +0600

    html

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,6 +1,5 @@
 import styles from "../../styles/Home.module.css";
 import logo from "../../image/logo.png";
-
 const Navigation = () => {
   return (
     <div className={styles.NavLogo}>
@@ -12,11 +11,32 @@ const Navigation = () => {
           <h2>Dog Person</h2>
         </div>
       </div>
-      <div className={styles.Navbar}>
-        <ul>
+      <div className={styles2.nav_items}>
+        <ul >
           <li>Home</li>
-          <li>Shop</li>
-          <li><a href="/classify">Classify</a></li>
+          <li className={styles.shopNav}>Shop
+            <ul className={styles.ProductType}>
+              <li className={styles.medi}>Medicine
+                <ul className={styles.MedicineType}>
+                  <li>Antibiotics</li>
+                  <li>Antifungal</li>
+                  <li>Antiparasitic</li>
+                  <li>Steriods</li>
+                  <li>Pain Reliever</li>
+                </ul>
+              </li>
+              <li className={styles.food}>Food
+                <ul className={styles.FoodType}>
+                  <li>Kibble</li>
+                  <li>Canned</li>
+                  <li>Raw</li>
+                  <li>Semi-Moist</li>
+                  <li>Home Made</li>
+                </ul>
+              </li>
+            </ul></li>
+          <li className={styles.classifyNav}>Classify
+          </li>
           <button className={styles.loginbtn}>Log In</button>
         </ul>
       </div>

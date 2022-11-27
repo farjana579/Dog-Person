bba5a999423fe865b3e902eac400849a3ee37bc0
farjanaabedin <farjanaabedin579@gmail.com>
2022-11-27 11:45:47 +0600

    Registration done

---
+++ components/Registration/RegisterComponent.js
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from "../../image/pawBlack.png";
+import styles from "../../styles/registration.module.css";
+const RegisterComponent = () => {
+  return (
+    <div>
+      <div className={styles.registrationHeader}>
+        <img src={logo.src}></img>
+        <h1>Dog Person</h1>
+      </div>
+      <div className={styles.registrationBox}>
+        <form>
+          <input type="text" id="fname" name="fname" placeholder="Name"></input>
+          <input type="text" id="Email" name="Email" placeholder="Email" />
+
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Retype Password"
+          />
+          <a>
+            <i>Forget password?</i>
+          </a>
+          <button className={styles.btnStyle}>LOG IN</button>
+          <a href="/registration">
+            Already have an account?. go to login page.
+          </a>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterComponent;

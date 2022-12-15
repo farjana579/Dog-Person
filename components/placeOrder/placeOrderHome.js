34e0d84fa37acc5f8dd42544a65a9018c35e212d
EituPaul <zitupaul@gmail.com>
2022-12-15 22:08:12 +0600

    place Order

--- components/placeOrder/placeOrderHome.js
+++ components/placeOrder/placeOrderHome.js
@@ -4,7 +4,8 @@ import Food from '../../image/food 1.png'
 
 const PlaceOrderHome = () => {
     return (
-        <div>
+        <div className={PlaceOrderStyle.between_first_second_div_and_sideDiv} >
+            <div className={PlaceOrderStyle.first_second_div}>
             <div className={PlaceOrderStyle.firstDiv}>
                 <h3>Delivered </h3>
                 <h2>Address</h2>
@@ -58,48 +59,51 @@ const PlaceOrderHome = () => {
                         </select>
                     </div>
                 </div>
+            </div> 
+                <div className={PlaceOrderStyle.SecondDiv}>
+                    <h2>Products</h2>
+
+                    <div className={PlaceOrderStyle.innerDiv}>
+                        <div className={PlaceOrderStyle.innerDivparent}>
+                            <img src={Food.src}></img>
+
+                            <div className={PlaceOrderStyle.innerDivWrite} >
+                                <h3>Purina Friskies Balance Dog Food with Chicken and Vegetables </h3>
+                                <p>Brand: <span>Purnima Friskies</span> </p>
+                                <p>Quantity:<span>5</span> </p>
+                                <p>Price:<span>300</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={PlaceOrderStyle.innerDiv}>
+                        <div className={PlaceOrderStyle.innerDivparent}>
+                            <img src={Food.src}></img>
+
+                            <div className={PlaceOrderStyle.innerDivWrite} >
+                                <h3>Purina Friskies Balance Dog Food with Chicken and Vegetables </h3>
+                                <p>Brand: <span>Purnima Friskies</span> </p>
+                                <p>Quantity:<span>5</span> </p>
+                                <p>Price:<span>300</span></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={PlaceOrderStyle.innerDiv}>
+                        <div className={PlaceOrderStyle.innerDivparent}>
+                            <img src={Food.src}></img>
+
+                            <div className={PlaceOrderStyle.innerDivWrite} >
+                                <h3>Purina Friskies Balance Dog Food with Chicken and Vegetables </h3>
+                                <p>Brand: <span>Purnima Friskies</span> </p>
+                                <p>Quantity:<span>5</span> </p>
+                                <p>Price:<span>300</span></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div> 
+           
             </div>
-            <div className={PlaceOrderStyle.SecondDiv}>
-                <h2>Products</h2>
-
-                <div className={PlaceOrderStyle.innerDiv}>
-                    <div className={PlaceOrderStyle.innerDivparent}>
-                        <img src={Food.src}></img>
-
-                        <div className={PlaceOrderStyle.innerDivWrite} >
-                            <h3>Purina Friskies Balance Dog Food with Chicken and Vegetables </h3>
-                            <p>Brand: <span>Purnima Friskies</span> </p>
-                            <p>Quantity:<span>5</span> </p>
-                            <p>Price:<span>300</span></p>
-                        </div>
-                    </div>
-                </div>
-                <div className={PlaceOrderStyle.innerDiv}>
-                    <div className={PlaceOrderStyle.innerDivparent}>
-                        <img src={Food.src}></img>
-
-                        <div className={PlaceOrderStyle.innerDivWrite} >
-                            <h3>Purina Friskies Balance Dog Food with Chicken and Vegetables </h3>
-                            <p>Brand: <span>Purnima Friskies</span> </p>
-                            <p>Quantity:<span>5</span> </p>
-                            <p>Price:<span>300</span></p>
-                        </div>
-                    </div>
-                </div>
-                <div className={PlaceOrderStyle.innerDiv}>
-                    <div className={PlaceOrderStyle.innerDivparent}>
-                        <img src={Food.src}></img>
-
-                        <div className={PlaceOrderStyle.innerDivWrite} >
-                            <h3>Purina Friskies Balance Dog Food with Chicken and Vegetables </h3>
-                            <p>Brand: <span>Purnima Friskies</span> </p>
-                            <p>Quantity:<span>5</span> </p>
-                            <p>Price:<span>300</span></p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+           
             <div className={PlaceOrderStyle.SideDiv}  >
                 <h2>Order Summery</h2>
                 <div className={PlaceOrderStyle.SideDivWrite} >
@@ -113,7 +117,8 @@ const PlaceOrderHome = () => {
                     <p>Total:<span>300</span></p>
                 </div>
                 <button className={PlaceOrderStyle.orderbtn}>Place Order</button>
-            </div>
+            </div>  
+           
         </div >
     );
 };
